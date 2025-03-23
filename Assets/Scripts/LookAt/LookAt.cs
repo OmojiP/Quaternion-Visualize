cfe35a0b22d34947d55b9dac56d4185a43c72cb6
OmojiP <kodaiorient612@gmail.com>
2025-03-23 20:15:23 +0900

    lookat slerp

--- Assets/Scripts/LookAt/LookAt.cs
+++ Assets/Scripts/LookAt/LookAt.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LookAt : MonoBehaviour
@@ -15,6 +13,27 @@ public class LookAt : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(target, Vector3.up);
+        // transform.LookAt(target, Vector3.up);
+        MyLookAt(target.position);
+    }
+
+    void MyLookAt(Vector3 target)
+    {
+        Vector3 pos = transform.position;
+
+        Vector3 r = (target - pos);
+
+        float planeAngle = -Mathf.Atan2(r.z, r.x);
+
+        var q1 = MyQuaternion.Unit(Vector3.up, planeAngle);
+
+        float otherAngle = Mathf.Atan2(r.y, new Vector2(r.x, r.z).magnitude);
+        Vector3 otherAxis = new Vector3(Mathf.Sin(otherAngle), 0, Mathf.Cos(otherAngle));
+
+        var q2 = MyQuaternion.Unit(Vector3.forward, otherAngle);
+
+        var q = q1 * q2;
+
+        transform.rotation = new Quaternion(x: q.x, y: q.y, z: q.z, w:q.w);
     }
 }
---
+++ Assets/Scripts/Slerp/MySlerp.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class MySlerp : MonoBehaviour
+{
+    [SerializeField] bool isSlerp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sl = isSlerp ? Slerp : Lerp;
+        SLStart(destroyCancellationToken).Forget();
+    }
+
+    Func<Quaternion, Quaternion, float, Quaternion> sl;
+
+    async UniTask SLStart(CancellationToken token)
+    {
+        Quaternion q0 = Quaternion.Euler(0, 0, 0);  // 開始回転
+        Quaternion q1 = Quaternion.Euler(0, 0, -180); // 終了回転
+
+        while(true)
+        {
+            await UniTask.Delay(500, cancellationToken: token);
+            await Lerping(q0, q1, token);
+        }
+    }
+
+    private async UniTask Lerping(Quaternion q0, Quaternion q1, CancellationToken token)
+    {
+        float t = 0f;
+        while(true)
+        {
+            transform.rotation = sl.Invoke(q0, q1, t);
+
+            if(t >= 1) break;
+
+            t += 0.0125f;
+            await UniTask.Delay(25, cancellationToken: token);
+        }
+    }
+
+
+    Quaternion Slerp(Quaternion q0, Quaternion q1, float t)
+    {
+        float dot = Quaternion.Dot(q0, q1);
+        
+        // 内積が負なら符号反転（最短経路にする）
+        if (dot < 0.0f) {
+            q1 = new Quaternion(-q1.x, -q1.y, -q1.z, -q1.w);
+            dot = -dot;
+        }
+        
+        float theta = Mathf.Acos(dot);
+        float sinTheta = Mathf.Sin(theta);
+
+        float w0 = Mathf.Sin((1 - t) * theta) / sinTheta;
+        float w1 = Mathf.Sin(t * theta) / sinTheta;
+
+        return new Quaternion(
+            w0 * q0.x + w1 * q1.x,
+            w0 * q0.y + w1 * q1.y,
+            w0 * q0.z + w1 * q1.z,
+            w0 * q0.w + w1 * q1.w
+        );
+    }
+    Quaternion Lerp(Quaternion q0, Quaternion q1, float t)
+    {
+        float dot = Quaternion.Dot(q0, q1);
+        
+        // 内積が負なら符号反転（最短経路にする）
+        if (dot < 0.0f) {
+            q1 = new Quaternion(-q1.x, -q1.y, -q1.z, -q1.w);
+            dot = -dot;
+        }
+        
+        float theta = Mathf.Acos(dot);
+        float sinTheta = Mathf.Sin(theta);
+
+        float w0 = (1 - t);
+        float w1 = t;
+
+        return new Quaternion(
+            w0 * q0.x + w1 * q1.x,
+            w0 * q0.y + w1 * q1.y,
+            w0 * q0.z + w1 * q1.z,
+            w0 * q0.w + w1 * q1.w
+        ).normalized;
+    }
+
+}
